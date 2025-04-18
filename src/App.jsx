2d35766ef228c79fd8901a3ee9fc31f1ac2fced7
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-04-18 20:18:25 +0100

    Genesis

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1>TEST</h1>
+      <Button className="bg-destructive" variant="secondary">Testing Shadcn and tailwind!</Button>
+    </>
+  )
+}
+
+export default App
