7921bc0431a82c3ff491de707815a1fb04693084
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-04-19 00:54:15 +0100

    Fixed header import case sensitive bug

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import Header from "@/components/header/header";
+import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
 
 const Layout = () => {
